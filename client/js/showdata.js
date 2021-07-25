b6a20efe80cf1918b1ff6b154a04e571b3f60ef6
Daniel Assayag <danass@protonmail.com>
2021-07-25 19:26:03 +0100

    all data parsed? html formatting remain

--- client/js/showdata.js
+++ client/js/showdata.js
@@ -1,17 +1,32 @@
 Template.simplified.onCreated(function () {})
 Template.simplified.helpers({ data() { return localdb.find({}) } })
 
-Template.registerHelper('table', function recur(o) {
+Template.registerHelper('table', function recur(o, bool) {
+  
+  if (bool == true && o instanceof Object ) {
+    console.log("allo", o)
+    let entries = Object.entries(o).map(entries => {
+      let key = entries[0]
+      let value = entries[1]
+      return HTML.TD(
+              HTML.B(
+                key),
+                value instanceof Object ? recur(value, true): HTML.TD(value)  )
+    })
+   return HTML.TD(entries)
+  }
+
   let entries = Object.entries(o).map(entries => {
     let key = entries[0]
     let value = entries[1]
     return HTML.TD(
       HTML.B(
         key),
-     value instanceof Object ? recur(value): HTML.TD(value)  
+     value instanceof Object ? recur(value, true): HTML.TD(value)  
     //  value instanceof Array ? "WAAYAY Array": ""
     )
   })
+  
   return HTML.toHTML(HTML.TR(entries))
 
 })
