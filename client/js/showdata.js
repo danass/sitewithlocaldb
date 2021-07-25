007ffaea57d66db17fc5eb950e576424177b3aa5
Daniel Assayag <danass@protonmail.com>
2021-07-25 21:07:45 +0100

    a bit better but still not as should be. improved html parsing.

--- client/js/showdata.js
+++ client/js/showdata.js
@@ -4,29 +4,36 @@ Template.simplified.helpers({ data() { return localdb.find({}) } })
 Template.registerHelper('table', function recur(o, bool) {
   
   if (bool == true && o instanceof Object ) {
-    console.log("allo", o)
-    let entries = Object.entries(o).map(entries => {
-      let key = entries[0]
-      let value = entries[1]
-      return HTML.TD(
-              HTML.B(
-                key),
-                value instanceof Object ? recur(value, true): HTML.TD(value)  )
+    let entries = Object.entries(o).map(entry => {
+      let key = entry[0]
+      let value = entry[1]
+      return (
+
+        HTML.TR({class: 'tuple'},
+           HTML.TD({'class': 'item'}, HTML.B(key)),
+           HTML.TD({'class': 'item'}, (value instanceof Object)? recur(value, true): value )  )
+           
+    )
     })
    return HTML.TD(entries)
   }
 
-  let entries = Object.entries(o).map(entries => {
-    let key = entries[0]
-    let value = entries[1]
-    return HTML.TD(
-      HTML.B(
-        key),
-     value instanceof Object ? recur(value, true): HTML.TD(value)  
-    //  value instanceof Array ? "WAAYAY Array": ""
+  let entries = Object.entries(o).map(entry => {
+    
+    let key = entry[0]
+    let value = entry[1]
+
+      console.log(entry,"bitch")
+    return (
+        HTML.TR({class: 'tuple'},
+           HTML.TD({'class': 'item'}, HTML.B(key)),
+           HTML.TD({'class': 'item'}, (value instanceof Object)? recur(value, true): value )  )
     )
+    
   })
-  
-  return HTML.toHTML(HTML.TR(entries))
+  render = HTML.toHTML( HTML.TABLE(entries))
+    console.log(render)
+  return render
+
 
 })
